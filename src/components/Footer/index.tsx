b42bebd309f3a6a5085cac8d06628bf0e9e9077b
TubiOb <Tubiobaloluwa@gmail.com>
2025-08-27 19:02:27 +0100

    made corrections to the routing of the footer.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ import React from "react"
 import { useEffect, useRef } from "react";
 import { Github, Linkedin, Twitter } from "lucide-react"
 import { motion } from "framer-motion";
-import Link from "next/link";
+// import Link from "next/link";
 import { usePathname } from 'next/navigation';
 // import { useAnimation } from "@/context/AnimationContext";
 import gsap from "gsap"
